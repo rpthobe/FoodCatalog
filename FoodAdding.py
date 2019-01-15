4e79ed138475d2a62e6e36e622372602f34110b0
Rosalie Thobe <rpthobe@gmail.com>
2019-01-15 13:50:55 -0500

    [1343534]First commit of project

---
+++ FoodAdding.py
@@ -0,0 +1,53 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+from database_setup import Course, FoodItem, Base
+
+engine = create_engine('sqlite:///mealsDB.db')
+# Bind the engine to the metadata of the Base class so that the
+# declaratives can be accessed through a DBSession instance
+Base.metadata.bind = engine
+
+DBSession = sessionmaker(bind=engine)
+# A DBSession() instance establishes all conversations with the database
+# and represents a "staging zone" for all the objects loaded into the
+# database session object. Any change made against the objects in the
+# session won't be persisted into the database until you call
+# session.commit(). If you're not happy about the changes, you can
+# revert all of them back to the last commit by calling
+# session.rollback()
+session = DBSession()
+
+
+# Establishing Meals
+course1 = Course(name="Breakfast")
+session.add(course1)
+session.commit()
+
+food1 = FoodItem(name="Pancakes",
+                 description="A fluffy circular sweet bread.",
+                 course=course1)
+
+session.add(food1)
+session.commit()
+
+course2 = Course(name="Lunch")
+session.add(course2)
+session.commit()
+
+food2 = FoodItem(name="Chicken sandwich",
+                 description="chicken with bread, lettuce, and tomato",
+                 course=course2)
+session.add(food2)
+session.commit()
+
+course3 = Course(name="Dinner")
+session.add(course3)
+session.commit()
+food3 = FoodItem(name="Mac and Cheese",
+                 description="Pasta with 4 types of cheeses",
+                 course=course3)
+session.add(food2)
+session.commit()
+
+print "added menu items to lunch!"
---
+++ database_setup.py
@@ -0,0 +1,69 @@
+from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+from sqlalchemy import create_engine
+
+Base = declarative_base()
+
+class UserTable(Base):
+    __tablename__ = 'users'
+    
+    id = Column(Integer, primary_key=True)
+    username = Column(String(80))
+    email = Column(String(80), nullable=False)
+    @property
+    def serialize(self):
+        """Return object data in easily serializeable format"""
+        return {
+            'id': self.id,
+            'username': self.username,
+        }
+
+
+class Course(Base):
+    __tablename__ = 'course'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable=False)
+    user_id = Column(Integer, ForeignKey('users.id'))
+    user = relationship(UserTable)
+    food_item = relationship('FoodItem', cascade='all, delete-orphan')
+    
+    
+    @property
+    def serialize(self):
+        """Return object data in easily serializeable format"""
+        return {
+            'name': self.name,
+            'id': self.id,
+            'user_id': self.user_id,
+        }
+
+
+class FoodItem(Base):
+    __tablename__ = 'food_item'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String(80), nullable=False)
+    description = Column(String(250))
+    user_id = Column(Integer, ForeignKey('users.id'))
+    user = relationship(UserTable)
+    course_id = Column(Integer, ForeignKey('course.id'))
+    course = relationship(Course)
+
+    @property
+    def serialize(self):
+        """Return object data in easily serializeable format"""
+        return {
+            'name': self.name,
+            'description': self.description,
+            'id': self.id,
+            'course_id': self.course_id,
+            'user_id': self.user_id,
+        }
+
+
+engine = create_engine('sqlite:///mealsDB.db')
+
+
+Base.metadata.create_all(engine)
